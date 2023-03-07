6d10db5b58e8e399ac1c1705bc076aed3d3fd06c
Bonnie-Zhu <124389118+Bonnie-Zhu@users.noreply.github.com>
2023-03-07 17:52:47 +0000

    done index,css,js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Bonnie ZHu
+// Created on: Mach 2023
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "Hello, World!"
+}
